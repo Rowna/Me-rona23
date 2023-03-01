4a8813d3c9ee44d624621ae9d6a01c23e2509a24
Rowna <rowan.kh1@gmail.com>
2023-03-01 12:44:21 +0100

    w: aboutMe & bars

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  //do work
 
-  // let dot1 = document.querySelector(".dot1");
+  let aboutMeScroll = document.querySelector(".aboutMe3__scroll");
+  document.addEventListener("", ()=> {
+
+  })
+
+  let dot1 = document.querySelector(".dot1");
   // let dot2 = document.querySelector(".dot2");
   // let dot3 = document.querySelector(".dot3");
-
+  console.log(dot1);
   // let progressValue = document.querySelectorAll(".aboutMe__progress-value");
 
   let progressValue1 = document.getElementById("value75"); // die 75 in '75%'
@@ -29,21 +33,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
   //     }
   //   }, speed);
   /**/
+
   const circles = [
     { id: progressValue1, offset: 0, target: 75, delay: 30 },
     { id: progressValue2, offset: 0, target: 80, delay: 32 },
     { id: progressValue3, offset: 0, target: 50, delay: 20 },
-    { id: progressValue4, offset: 0, target: 35, delay: 20 },
+    { id: progressValue4, offset: 0, target: 35, delay: 17 },
   ];
 
   circles.forEach((circle) => {
-    console.log(circle);
+    console.log("Circles Values rolling!");
     onLoadingTime(circle);
     // verarbeitung und steuerung im DOM
   });
 });
 
 function onLoadingTime(circle) {
+  // setTimeout runs code only once
   let timeOut = setInterval(() => {
     // if (circle.id.innerHTML == circle.target) {
     circle.offset++;
@@ -53,6 +59,59 @@ function onLoadingTime(circle) {
       clearInterval(timeOut);
     }
   }, circle.delay);
-
-  //
 }
+
+/*
+// if (progressValue1.innerHTML == progressEndValue1) {
+//   // setTimeOut
+//   const progress1 = setInterval(() => {
+//     progressStartValue++;
+
+//     progressValue1.textContent = `${progressStartValue}`;
+//     if (progressStartValue == progressEndValue1) {
+//       clearInterval(progress1);
+//     }
+//   }, speed);
+*/
+
+// let circle75 = document.querySelector(".circlee");
+// console.log(circle75)
+// let progressValue1 = document.querySelector(".value75");  // die 75 in '75%'
+
+let skillsSection = document.getElementById("aboutMe__skills-container");
+window.addEventListener("scroll", () => {
+  // Treffpunkt von position des Elements "skillsSection" und
+  // screenposition "windows.innerHeight",
+  // section <kleiner als screen
+
+  const sectionPosition = skillsSection.getBoundingClientRect().top;
+  const screenposition = window.innerHeight;
+  /* 
+  function showProgress(params) {
+  progressBarsAll.forEach((progressBar) => {
+  const progressValue = progressBar.dataset.progress;
+  circles.forEach((circle) => {
+  const setTime = setTimeout(() => {
+  onLoadingTime(circle);
+  console.log(circle.);
+  }, 100);
+  clearTimeout(setTime);
+  });
+  circle75.classList.add("cirlce75");
+  }
+  function hideProgress(params) {
+  circle75.classList.remove("cirlce75");
+  }
+  */
+  if (sectionPosition < screenposition) {
+    // console.log("skillsSection is klein: " + sectionPosition);
+    // showProgress();
+    // dot1.classList.add("dot1");
+    console.log("...");
+  } else {
+    // hideProgress();
+    console.log(".344.");
+
+    // console.log("screenPosition is big: " + screenPosition);
+  }
+});
